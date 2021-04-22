62326c5adbd2f99ffe29ee4307915e5bc707121f
Alexander Young <alex.buzov@icloud.com>
2021-04-22 13:03:22 -0700

    Kelvin and HSBK (#5)
    * kelvin
    
    * Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,12 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Colors",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "Colors", targets: ["Colors"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         .target(name: "Colors", dependencies: []),
---
+++ Sources/Colors/Kelvin.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public typealias Kelvin = UInt16
+
+public extension Kelvin {
+    static let warm: Kelvin = 2500
+    static let neutral: Kelvin = 3500
+    static let cool: Kelvin = 9000
+}
